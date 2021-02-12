29871eeaa8de7ee0e0af026bf6adde1d07869e33
venkateshgaddam <venkateshgaddam29@gmail.com>
2021-02-12 14:45:42 +0530

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenerateInvoice
+{
+    public class Product
+    {
+        public string product { get; set; }
+        public string producttype { get; set; }
+        public bool Isimported { get; set; }
+    }
+}
